032f2f6414e2d843141058095ddca0af7402ada0
fuadaishan <83373189+fuadaishan@users.noreply.github.com>
2024-12-11 23:05:33 +0300

    3

---
+++ WebProje/Models/ClinicDoctor.cs
@@ -0,0 +1,10 @@
+// namespace WebProje.Models;
+//
+// public class ClinicDoctor
+// {
+//     public int Id { get; set; }
+//     public int DoctorId { get; set; }
+//     public int ClinicId { get; set; }
+//     public Doctor Doctor { get; set; } = null!;
+//     public Clinic Clinic { get; set; } = null!;
+// }
